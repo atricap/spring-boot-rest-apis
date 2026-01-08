207b03528e716231133ad60fd2fbd38e6f6ef175
atricap <172540644+atricap@users.noreply.github.com>
2026-01-08 22:40:54 +0100

    Apply my code changes: Part 02a (UserDetailsManager) of 03-spring-boot-rest-security

--- 03-spring-boot-rest-security/employees/src/main/java/com/luv2code/springboot/employees/security/SecurityConfig.java
+++ 03-spring-boot-rest-security/employees/src/main/java/com/luv2code/springboot/employees/security/SecurityConfig.java
@@ -6,62 +6,36 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsManager() {
-//        UserDetails john = User.builder()
-//                .username("john")
-//                .password("{noop}test123")
-//                .roles("EMPLOYEE")
-//                .build();
-//
-//        UserDetails mary = User.builder()
-//                .username("mary")
-//                .password("{noop}test123")
-//                .roles("EMPLOYEE", "MANAGER")
-//                .build();
-//
-//        UserDetails susan = User.builder()
-//                .username("susan")
-//                .password("{noop}test123")
-//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(john, mary, susan);
-//    }
-
-//    @Bean
-//    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
-
-    // add support for JDBC ... no more hardcoded users :-)
-
     @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        var udm = new JdbcUserDetailsManager(dataSource);
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
 
-        // define query to retrieve a user by username
-        udm.setUsersByUsernameQuery(
-                "select user_id, password, active from system_users where user_id=?" );
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
 
-        // define query to retrieve the authorities/roles by username
-        udm.setAuthoritiesByUsernameQuery(
-                "select user_id, role from roles where user_id=?" );
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
 
-        return udm;
+        return new InMemoryUserDetailsManager(john, mary, susan);
     }
 
     @Bean
